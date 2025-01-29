c58932d5f2360378fb1241c48f2edec782f7c5b6
Aditya Pawar <aditya.pawar23624@gmail.com>
2025-01-29 15:55:09 +0530

    idek whats going on

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import MyContract from './contracts/MyContract.json';
 import './App.css';
 
 function App() {
+  const [account, setAccount] = useState('');
+  const [contract, setContract] = useState(null);
+
+  useEffect(() => {
+    const init = async () => {
+      const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
+      const accounts = await web3.eth.requestAccounts();
+      setAccount(accounts[0]);
+
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = MyContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        MyContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+      setContract(instance);
+    };
+
+    init();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Your account: {account}</p>
+        {/* Add more UI elements to interact with your contract */}
       </header>
     </div>
   );
